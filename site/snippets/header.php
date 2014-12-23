42940ee37277771bd4041f8cba40fc657ff184c0
Nathan Chapman <nathan@nchp.mn>
2014-12-23 20:57:37 +1100

    Add <meta> tags
    lol SEO

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -4,6 +4,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="<?php echo $site->description() ?>">
+    <meta name="author" content="<?php echo $site->author() ?>">
+    <meta name="keywords" content="<?php echo $site->keywords() ?>">
     <title>Nathan Chapman &ndash; Steadicam Operator</title>
 
     <!-- Bootstrap -->
