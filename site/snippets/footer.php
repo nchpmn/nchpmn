459141db7671af6f4916567b322da3a76e4cf320
Nathan Chapman <nathan@nchp.mn>
2014-12-23 16:33:57 +1100

    Add header and footer snippets
    Note: the .js files in the footer of design have to be included in each
    page's own template, to reduce loading of unused .js

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,14 +1,43 @@
-  <footer class="footer cf" role="contentinfo">
-
-    <div class="copyright">
-      <?php echo $site->copyright()->kirbytext() ?>
+    
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-5 col-sm-2 col-sm-offset-1">
+                    <p class="text-right">CONTACT ME</p>
+                </div>
+                <div class="col-xs-7 col-sm-4">
+                    <dl>
+                        <dt>Email</dt>
+                        <dd><a href="mailto:nathan@nchp.mn">nathan@nchp.mn</a></dd>
+                        <dt>Location</dt>
+                        <dd>Sydney &amp; Wollongong, Australia</dd>
+                        <dt>Phone</dt>
+                        <dd>+61 416 544 800</dd>
+                    </dl>
+                </div>
+                <div class="col-xs-4 hidden-xs">
+                    <p>SOCIALLY</p>
+                    <a href="http://youtube.com/nchpmn"><i class="fa fa-youtube-square fa-3x"></i></a>
+                    <a href="http://twitter.com/nchpmn"><i class="fa fa-twitter-square fa-3x"></i></a>
+                    <a href="http://facebook.com/nchpmn"><i class="fa fa-facebook-square fa-3x"></i></a>
+                    <a href="http://vimeo.com/nchpmn"><i class="fa fa-vimeo-square fa-3x"></i></a>
+                </div>
+				<div class="col-xs-5 visible-xs">
+                    <p class="text-right">SOCIALLY</p>
+                </div>
+                <div class="col-xs-7 visible-xs">
+                    <a href="http://youtube.com/nchpmn"><i class="fa fa-youtube-square fa-3x"></i></a>
+                    <a href="http://twitter.com/nchpmn"><i class="fa fa-twitter-square fa-3x"></i></a>
+                    <a href="http://facebook.com/nchpmn"><i class="fa fa-facebook-square fa-3x"></i></a>
+                    <a href="http://vimeo.com/nchpmn"><i class="fa fa-vimeo-square fa-3x"></i></a>
+                </div>
+            </div>
+        </div>
+    </footer>
+    
+    <div id="copyright">
+        <div class="container">
+            <p>Copyright Nathan Chapman &mdash; 2014</p>
+        </div>
     </div>
-
-    <div class="colophon">
-      <a href="http://getkirby.com/made-with-kirby-and-love">Made with Kirby and <b>♥</b></a>
-    </div>
-
-  </footer>
-
-</body>
-</html>
+    <!-- /footer -->
--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,22 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Nathan Chapman &ndash; Steadicam Operator</title>
 
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="fonts/flama.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/owl.carousel.css" rel="stylesheet">
 
-  <title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
-  <meta name="description" content="<?php echo $site->description()->html() ?>">
-  <meta name="keywords" content="<?php echo $site->keywords()->html() ?>">
-
-  <?php echo css('assets/css/main.css') ?>
-
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
 <body>
-
-  <header class="header cf" role="banner">
-    <a class="logo" href="<?php echo url() ?>">
-      <img src="<?php echo url('assets/images/logo.svg') ?>" alt="<?php echo $site->title()->html() ?>" />
-    </a>
-    <?php snippet('menu') ?>
-  </header>
