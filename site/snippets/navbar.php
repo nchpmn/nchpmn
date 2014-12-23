e38a42866caca76fda6f469799c509adba41f957
Nathan Chapman <nathan@nchp.mn>
2014-12-23 16:46:19 +1100

    Add snippets, blueprints

---
+++ site/snippets/navbar.php
@@ -0,0 +1,20 @@
+<!-- navbar -->
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#collapse">
+                <span class="sr-only">Toggle Navigation</span>
+                <i class="fa fa-bars"></i>
+            </button>
+            <a class="navbar-brand" href="#">NCHP<span class="orange"><strong>.</strong></span>MN</a>
+        </div>
+        <div class="collapse navbar-collapse clearfix" id="collapse">
+            <ul class="nav navbar-nav pull-right">
+                <li><a href="#cover">Home</a></li>
+                <li><a href="#work">Work</a></li>
+                <li><a href="#blog">Blog</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </div>
+    </div>
+</nav><!-- End navbar -->
