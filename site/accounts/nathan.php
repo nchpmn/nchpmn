9ab1270a2dbd9d6faf56b022a6845fc00db67609
Nathan Chapman <nathan@nchp.mn>
2015-01-23 09:32:17 +1100

    Add new blog post for testing

--- site/accounts/nathan.php
+++ site/accounts/nathan.php
@@ -6,7 +6,8 @@ password: >
   $2a$10$YMeRNrw0Z2shQlIph.YrMu.570D.3YjDXhgwdIAkcXs3E1SuPHzXm
 language: en
 role: admin
-token: 8add4d07818a52cbdf1d2dba499d883452b93315
+token: d7150cbfe39b51c0157764c739b0c4e1edc8eaa5
 history:
+  - blog/post-5
   - blog/jackie-chan-the-action-comedy
   - blog/doctor-karl
