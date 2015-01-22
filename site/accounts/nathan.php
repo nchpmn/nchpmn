2fa6c0fad3b0b7dc1f9bcb83439e9644f176627f
Nathan Chapman <nathan@nchp.mn>
2015-01-22 14:44:53 +1100

    Ah... token change?
    All I did was log on, I swear

--- site/accounts/nathan.php
+++ site/accounts/nathan.php
@@ -6,7 +6,7 @@ password: >
   $2a$10$YMeRNrw0Z2shQlIph.YrMu.570D.3YjDXhgwdIAkcXs3E1SuPHzXm
 language: en
 role: admin
-token: 3628609c9c934a81605521a42f798c7552b51aa0
+token: 8add4d07818a52cbdf1d2dba499d883452b93315
 history:
   - blog/jackie-chan-the-action-comedy
   - blog/doctor-karl
