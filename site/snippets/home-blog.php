0e812410df86c094e41d7ee45a2c0c56fb95c136
Nathan Chapman <nathan@nchp.mn>
2015-01-23 12:38:02 +1100

    Add actual #blog markup

--- site/snippets/home-blog.php
+++ site/snippets/home-blog.php
@@ -1,37 +1,41 @@
 <section id="blog">
     <h3 class="text-center"><a href="blog"><span>Blog</span></a></h3>
     <div class="owl-carousel">
-        <div class="item">First</div>
-        <div class="item">Second</div>
-        <div class="item">Third</div>
-        <div class="item">Fourth</div>
-        <div class="item">First</div>
-        <div class="item">Second</div>
-        <div class="item">Third</div>
-        <div class="item">Fourth</div>
-        <div class="item">First</div>
-        <div class="item">Second</div>
-        <div class="item">Third</div>
-        <div class="item">Fourth</div>
-        <div class="item">First</div>
-        <div class="item">Second</div>
-        <div class="item">Third</div>
-        <div class="item">Fourth</div>
-        <div class="item">First</div>
-        <div class="item">Second</div>
-        <div class="item">Third</div>
-        <div class="item">Fourth</div>
-        <div class="item">First</div>
-        <div class="item">Second</div>
-        <div class="item">Third</div>
-        <div class="item">Fourth</div>
-        <div class="item">First</div>
-        <div class="item">Second</div>
-        <div class="item">Third</div>
-        <div class="item">Fourth</div>
-        <div class="item">First</div>
-        <div class="item">Second</div>
-        <div class="item">Third</div>
-        <div class="item">Fourth</div>
+        <div class="item">
+            <a href="#">
+                <img class="img-responsive" src="http://placehold.it/1024x768/3D70B2/2462B2" alt="Blog">
+                <div class="blog-description">
+                    <h4>Blog Post Title</h4>
+                    <span>14 March 2014</span>
+                </div>
+            </a>
+        </div>
+        <div class="item">
+            <a href="#">
+                <img class="img-responsive" src="http://placehold.it/1024x768/FF6F8C/FF456B" alt="Blog">
+                <div class="blog-description">
+                    <h4>Why I hate AVCHD</h4>
+                    <span>23 August 2014</span>
+                </div>
+            </a>
+        </div>
+        <div class="item">
+            <a href="#">
+                <img class="img-responsive" src="http://placehold.it/1024x768/45B86B/18B84D" alt="Blog">
+                <div class="blog-description">
+                    <h4>YouTube, lol?</h4>
+                    <span>23 August 2014</span>
+                </div>
+            </a>
+        </div>
+        <div class="item">
+            <a href="#">
+                <img class="img-responsive" src="http://placehold.it/1024x768/E88652/E87335" alt="Blog">
+                <div class="blog-description">
+                    <h4>YouTube, lol?</h4>
+                    <span>23 August 2014</span>
+                </div>
+            </a>
+        </div>
     </div>
 </section><!-- End blog -->
