9fd9c53755643707b4f90ebf6baf76b0a7990691
Nathan Chapman <nathan@nchp.mn>
2015-01-23 12:41:00 +1100

    Change js variables

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -100,21 +100,11 @@
             });
             
             $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
-    }
-})
+                loop:true,
+                margin:10,
+                nav:true,
+                items:1,
+            })
         </script>
     <?php endif; ?>
 </body>
