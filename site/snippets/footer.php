7debea8b30fcad79a2ad8fc2060e5584042db1ad
Nathan Chapman <nathan@nchp.mn>
2015-01-23 12:37:24 +1100

    Basic Carousel
    It's working - so what the fuck was happening, then?

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -99,9 +99,22 @@
                 }); 
             });
             
-            $(document).ready(function(){
-                $(".owl-carousel").owlCarousel();
-            });
+            $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
         </script>
     <?php endif; ?>
 </body>
--- site/snippets/home-blog.php
+++ site/snippets/home-blog.php
@@ -1,9 +1,37 @@
 <section id="blog">
     <h3 class="text-center"><a href="blog"><span>Blog</span></a></h3>
     <div class="owl-carousel">
-        <div>First</div>
-        <div>Second</div>
-        <div>Third</div>
-        <div>Fourth</div>
+        <div class="item">First</div>
+        <div class="item">Second</div>
+        <div class="item">Third</div>
+        <div class="item">Fourth</div>
+        <div class="item">First</div>
+        <div class="item">Second</div>
+        <div class="item">Third</div>
+        <div class="item">Fourth</div>
+        <div class="item">First</div>
+        <div class="item">Second</div>
+        <div class="item">Third</div>
+        <div class="item">Fourth</div>
+        <div class="item">First</div>
+        <div class="item">Second</div>
+        <div class="item">Third</div>
+        <div class="item">Fourth</div>
+        <div class="item">First</div>
+        <div class="item">Second</div>
+        <div class="item">Third</div>
+        <div class="item">Fourth</div>
+        <div class="item">First</div>
+        <div class="item">Second</div>
+        <div class="item">Third</div>
+        <div class="item">Fourth</div>
+        <div class="item">First</div>
+        <div class="item">Second</div>
+        <div class="item">Third</div>
+        <div class="item">Fourth</div>
+        <div class="item">First</div>
+        <div class="item">Second</div>
+        <div class="item">Third</div>
+        <div class="item">Fourth</div>
     </div>
 </section><!-- End blog -->
