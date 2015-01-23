00b9cfee29c910bf6e671fecabd44de15d243225
Nathan Chapman <nathan@nchp.mn>
2015-01-23 14:51:45 +1100

    Testing Css-Tricks Responsive Video - FAIL

--- site/templates/project.php
+++ site/templates/project.php
@@ -1,11 +1,18 @@
 <?php snippet('header', array('bodyclass' => 'work-single')) ?>
 
-<header id="cover" class="jumbotron cover" 
+<header id="cover" class="jumbotron cover">
+    <div class="owl-carousel">
+        <?php if($page->video()): ?>
+            <div class="item">
+                <?php echo vimeo($page->video()) ?>
+            </div>
+        <?php endif ?>
+    </div>
     <?php $cover = $page->cover(); ?>
     <?php if($image = $page->image(strval($cover))): ?>
         style="background-image:url('<?php echo $image->url() ?>');"
     <?php endif ?>
-></header><!-- End header -->
+</header><!-- End header -->
 
 <article>
     <div class="container">
@@ -34,6 +41,44 @@
         </div>
     </div>
 </article>
+
+<script>
+// Find all YouTube videos
+var $allVideos = $("iframe[src^='http://player.vimeo.com'], iframe[src^='http://www.youtube.com']"),
+
+    // The element that is fluid width
+    $fluidEl = $("body");
+
+// Figure out and save aspect ratio for each video
+$allVideos.each(function() {
+
+  $(this)
+    .data('aspectRatio', this.height / this.width)
+
+    // and remove the hard coded width/height
+    .removeAttr('height')
+    .removeAttr('width');
+
+});
+
+// When the window is resized
+$(window).resize(function() {
+
+  var newWidth = $fluidEl.width();
+
+  // Resize all videos according to their own aspect ratio
+  $allVideos.each(function() {
+
+    var $el = $(this);
+    $el
+      .width(newWidth)
+      .height(newWidth * $el.data('aspectRatio'));
+
+  });
+
+// Kick off one resize to fix all videos on page load
+}).resize();
+</script>
 
 
 <?php snippet('prevnext') ?>
