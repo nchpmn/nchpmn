c210554ebf9062c71ac81aa9d0fb7fee81f382c2
Nathan Chapman <nathan@nchp.mn>
2015-01-23 12:56:53 +1100

    Add foreach loop, kirby markup

--- site/snippets/home-blog.php
+++ site/snippets/home-blog.php
@@ -1,41 +1,18 @@
 <section id="blog">
     <h3 class="text-center"><a href="blog"><span>Blog</span></a></h3>
     <div class="owl-carousel">
-        <div class="item">
-            <a href="#">
-                <img class="img-responsive" src="http://placehold.it/1024x768/3D70B2/2462B2" alt="Blog">
-                <div class="blog-description">
-                    <h4>Blog Post Title</h4>
-                    <span>14 March 2014</span>
-                </div>
-            </a>
-        </div>
-        <div class="item">
-            <a href="#">
-                <img class="img-responsive" src="http://placehold.it/1024x768/FF6F8C/FF456B" alt="Blog">
-                <div class="blog-description">
-                    <h4>Why I hate AVCHD</h4>
-                    <span>23 August 2014</span>
-                </div>
-            </a>
-        </div>
-        <div class="item">
-            <a href="#">
-                <img class="img-responsive" src="http://placehold.it/1024x768/45B86B/18B84D" alt="Blog">
-                <div class="blog-description">
-                    <h4>YouTube, lol?</h4>
-                    <span>23 August 2014</span>
-                </div>
-            </a>
-        </div>
-        <div class="item">
-            <a href="#">
-                <img class="img-responsive" src="http://placehold.it/1024x768/E88652/E87335" alt="Blog">
-                <div class="blog-description">
-                    <h4>YouTube, lol?</h4>
-                    <span>23 August 2014</span>
-                </div>
-            </a>
-        </div>
-    </div>
+        <?php foreach(page('blog')->children()->visible()->flip()->limit(3) as $item): ?>
+            <div class="item">
+                <a href="<?php echo $item->url() ?>">
+                    <?php $cover = $item->cover(); ?>
+                    <?php if($image = $item->image(strval($cover))): ?>
+                        <img class="img-responsive" src="<?php echo $image->url() ?>">
+                    <?php endif ?>
+                    <div class="blog-description">
+                        <h4><?php echo $item->title() ?></h4>
+                        <span><?php echo $item->date('j M Y') ?></span>
+                    </div>
+                </a>
+            </div>
+        <?php endforeach ?>
 </section><!-- End blog -->
