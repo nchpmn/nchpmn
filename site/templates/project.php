7ab9b32db11cfadb3781e25aee6aad4243066029
Nathan Chapman <nathan@nchp.mn>
2015-02-01 16:11:35 +1100

    Remove testing code

--- site/templates/project.php
+++ site/templates/project.php
@@ -1,8 +1,8 @@
 <?php snippet('header', array('bodyclass' => 'work-single')) ?>
 
 <header id="cover" class="jumbotron cover owl-carousel">
-    <div class="item video-responsive">
-        <iframe src="//player.vimeo.com/video/114730775" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+    <div class="item">
+    
     </div>
     <?php foreach($page->files() as $image): ?>
         <div class="item">
@@ -13,45 +13,6 @@
 </header>
 
 <?php snippet('vimeothumb', array('id' => 118367247)) ?>
-
-<!--
-jQuery(document).ready(function() {
-    jQuery("input#vimsub").click(function() {
-        var vimeoVideoIDraw = jQuery("input#vimeoid").val();
-        var vimeoVideoID = jQuery.trim(vimeoVideoIDraw);
-        jQuery.getJSON('http://vimeo.com/api/v2/video/' + vimeoVideoID + '.json?callback=?', {
-            format: "json"
-        }, function(data, status) {
-            var title = data[0].title;
-            var videourl = data[0].url;
-            var thisuser = data[0].user_name;
-            var userurl = data[0].user_url;
-            var largeThumb = data[0].thumbnail_large;
-            jQuery("p.large a").attr('href', largeThumb).text(largeThumb);
-            jQuery(".largethumb").attr('src', largeThumb);
-            jQuery("a.video").attr('href', videourl).text(title);
-            jQuery("a.user").attr('href', userurl).text(thisuser);
-            jQuery("#result").addClass("visible");
-            var raw = largeThumb.split('_');
-            var urlsnip = raw[0];
-            var hdThumb = urlsnip + '_1280.jpg';
-            jQuery("p.hd a").attr('href', hdThumb).text(hdThumb);
-            jQuery(".hdthumb").attr('src', hdThumb);
-            jQuery(".hdthumb").error(function() {
-                jQuery(".hdthumb").hide();
-                jQuery("p.hd a").hide();
-                jQuery("p.hd").addClass("nohd").text("There is no HD-Thumnail for this Video.");
-            });
-            jQuery("footer").addClass("visible");
-        });
-    });
-});
-if (!elementSupportsAttribute('input', 'autofocus')) {
-    document.getElementById('vimeoid').focus();
-}
-
-
--->
 
 <article>
     <div class="container">
