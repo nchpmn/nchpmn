44a3ceaee6f3d790b4806c85e0778940e2866adf
Nathan Chapman <nathan@nchp.mn>
2015-02-09 18:00:23 +1100

    Remove dup content

--- site/accounts/nathan.php
+++ site/accounts/nathan.php
@@ -6,7 +6,7 @@ password: >
   $2a$10$YMeRNrw0Z2shQlIph.YrMu.570D.3YjDXhgwdIAkcXs3E1SuPHzXm
 language: en
 role: admin
-token: e83720a3661abfae5f0a67acd435a78ee2c4d108
+token: 4f6e8af89d0c2e8d48aab8d8b421d9b88976ad62
 history:
   - work/test
   - work/air-nz
