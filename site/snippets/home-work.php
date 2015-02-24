c34b177a823d3d33948c40c3a704c845f15c5ce6
Nathan Chapman <nathan@nchp.mn>
2015-02-25 10:58:49 +1100

    Add media to Colio.js areas

--- site/snippets/home-work.php
+++ site/snippets/home-work.php
@@ -8,24 +8,24 @@
                 <a href="#" data-filter=".tvc" class="btn btn-link btn-lg">TVC</a>
                 <a href="#" data-filter=".sport" class="btn btn-link btn-lg">Sport</a>
             </div>
-            
-            <div class="portfolioContainer">
-                <?php foreach(page('work')->children()->visible()->flip()->limit(9) as $item): ?>
-                    <div class="<?php echo $item->category() ?> col-xs-6 col-lg-4" data-content="<?php echo $item->url() ?>">
-                        <a class="colio-link" href="#">
-                            <div class="work-description text-center">
-                                <h4><?php echo $item->title() ?></h4>
-                                <span><?php echo $item->role() ?></span>
-                            </div>
-                            <?php $cover = $item->cover(); ?>
-                            <?php if($image = $item->image(strval($cover))): ?>
-                                <img class="img-responsive" src="<?php echo $image->url() ?>">
-                            <?php endif ?>
-                        </a>
-                    </div>
-                <?php endforeach ?>
-            </div>
-            
         </div>
+            
+        <div class="portfolioContainer">
+            <?php foreach(page('work')->children()->visible()->flip()->limit(9) as $item): ?>
+                <div class="<?php echo $item->category() ?> col-xs-6 col-lg-4" data-content="<?php echo $item->url() ?>">
+                    <a class="colio-link" href="#">
+                        <div class="work-description text-center">
+                            <h4><?php echo $item->title() ?></h4>
+                            <span><?php echo $item->role() ?></span>
+                        </div>
+                        <?php $cover = $item->cover(); ?>
+                        <?php if($image = $item->image(strval($cover))): ?>
+                            <img class="img-responsive" src="<?php echo $image->url() ?>">
+                        <?php endif ?>
+                    </a>
+                </div>
+            <?php endforeach ?>
+        </div>
+
     </div>
 </section><!-- End work -->
--- site/templates/project.php
+++ site/templates/project.php
@@ -1,10 +1,28 @@
-    <div class="col-sm-5">
-        <?php $cover = $page->cover(); ?>
-        <?php if($image = $page->image(strval($cover))): ?>
-            <img class="img-responsive" src="<?php echo $image->url() ?>">
-        <?php endif ?>
+<div class="row">
+    <div class="col-sm-7" style="display:block">
+        <?php if($page->videoSource() == "youtube"): ?>
+        <!-- YOUTUBE -->
+        <div class="video-responsive item">
+            <iframe width="1280" height="720" src="https://www.youtube.com/embed/<?php echo $page->videoID(); ?>?showinfo=0&autohide=1&autoplay=0&controls=2&modestbranding=1" frameborder="0" allowfullscreen></iframe>
+        </div>
+    <?php elseif($page->videoSource() == "vimeo"): ?>
+        <!-- VIMEO -->
+        <div class="video-responsive item">
+            <iframe src="http://player.vimeo.com/video/<?php echo $page->videoID(); ?>?portrait=0&title=0&badge=0&byline=0&color=ed8046" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+        </div>    
+    <?php endif; ?>
+    
+    <?php foreach($page->projectImages()->split() as $image): ?>
+        <div class="item">
+            <img class="img-responsive" src="<?php echo $page->files()->get($image)->url() ?>">
+        </div>
+    <?php endforeach ?>
     </div>
-    <div class="col-sm-7">
+    <div class="col-sm-5">
         <h1><?php echo $page->title() ?></h1>
+        <span class="pull-left"><?php echo $page->role() ?></span>
+        <span class="pull-right"><?php echo $page->date('Y', 'year')?></span>
+        
         <?php echo $page->text()->kirbytext() ?>
     </div>
+</div>
