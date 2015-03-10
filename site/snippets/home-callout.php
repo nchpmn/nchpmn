6ad6d61d2d8b4a8e8129f3e7dafe9315c5b2e154
Nathan Chapman <nathan@nchp.mn>
2015-03-10 19:15:49 +1100

    Change home-callout.php to child of home.php

--- site/snippets/home-callout.php
+++ site/snippets/home-callout.php
@@ -1,8 +1,13 @@
-<section id="callout">
+<section id="callout" style="background-image:url(
+        <?php $photo = $data->photo(); ?>
+        <?php if($image = $data->image(strval($photo))): ?>
+            <?php echo $image->url() ?>
+        <?php endif ?>
+        )">
     <div class="container">
         <div class="row text-center">
-            <h4>Want Me On Set?</h4>
-            <a class="btn btn-primary" href="#contact">Contact Me</a>
+            <h4><?php echo $data->header() ?></h4>
+            <a class="btn btn-primary" href="<?php echo $data->buttonURL() ?>"><?php echo $data->button() ?></a>
         </div>
     </div>
 </section><!-- End callout -->
