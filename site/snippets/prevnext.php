cfac058c1b199bdf9009d31b6a16528700e62b76
Nathan Chapman <nathan@nchp.mn>
2015-03-10 16:28:11 +1100

    Change sides Prev/Next links were on
    "Next", being the future, should be on the right. Converse is true for
    "Previous"

--- site/snippets/prevnext.php
+++ site/snippets/prevnext.php
@@ -2,13 +2,13 @@
     <div class="container">
         <hr />
         <div class="row">
-            
-            <?php if($page->hasNextVisible()): ?>
-                <a class="next col-xs-3 col-xs-offset-3" href="<?php echo $page->nextVisible()->url() ?>">NEXT</a>
-            <?php endif ?>
             <?php if($page->hasPrevVisible()): ?>
-                <a class="prev col-xs-3 <?php if($page->hasNextVisible()): ?><?php else: ?>col-xs-offset-6<?php endif; ?>" href="<?php echo $page->prevVisible()->url() ?>"><span class="hidden-xs">PREVIOUS</span><span class="visible-xs">PREV</span></a>
+                <a class="next col-xs-3 col-xs-offset-3" href="<?php echo $page->prevVisible()->url() ?>"><span class="hidden-xs">PREVIOUS</span><span class="visible-xs">PREV</span></a>
             <?php endif ?>
+            <?php if($page->hasNextVisible()): ?>
+                <a class="prev col-xs-3 <?php if($page->hasNextVisible()): ?><?php else: ?>col-xs-offset-6<?php endif; ?>" href="<?php echo $page->nextVisible()->url() ?>">NEXT</a>
+            <?php endif ?>
+            
         </div>
     </div>
 </section>
