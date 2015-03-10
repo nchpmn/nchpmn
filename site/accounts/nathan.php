1fb35ebf5efb3483003406c0ab809ab33f8dbad0
Nathan Chapman <nathan@nchp.mn>
2015-03-10 19:08:50 +1100

    Add tag support to home-work.php

--- site/accounts/nathan.php
+++ site/accounts/nathan.php
@@ -8,8 +8,8 @@ language: en
 role: admin
 token: 72d2958b1172c1dcbf88580f9974b99b1503600c
 history:
-  - work/test-2
+  - home/work
   - work/test
   - work/air-nz
+  - work/test-2
   - work/project-c
-  - work/project-b
--- site/blueprints/home-work.php
+++ site/blueprints/home-work.php
@@ -6,6 +6,10 @@ fields:
   title:
     label: Title
     type:  text
+  tags:
+    label: Tags
+    type: tags
+    index: all
   number:
     label: Items to Show
     type: number
