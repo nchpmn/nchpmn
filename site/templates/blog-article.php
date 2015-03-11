9e8be07bc6847ded6d9953d1be7f970e7a06db93
Nathan Chapman <nathan@nchp.mn>
2015-03-12 10:40:09 +1100

    blog-article.php basic layout

--- site/templates/blog-article.php
+++ site/templates/blog-article.php
@@ -1,3 +1,23 @@
-<?php snippet('header', array('bodyclass' => 'home')) ?>
+<?php snippet('header', array('bodyclass' => 'blog-article')) ?>
 
+<article>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
+                <h1><?php echo $page->title() ?></h1>
+                <div class="meta col-xs-10 col-xs-offset-1">
+                    <span><?php echo $page->date('j M Y') ?> &mdash; <?php snippet('readingtimeshort', array('text' => $page->text())) ?></span>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
+                <div class="leading">
+                    <?php echo $page->excerpt()->kirbytext() ?>
+                </div>
+                <?php echo $page->text()->kirbytext() ?>
+            </div>            
+        </div>    
+    </div>
+</article>
 <?php snippet('footer') ?>
