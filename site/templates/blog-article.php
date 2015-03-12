990fd723d27046f5db855fe2c9a820c39205d8d1
Nathan Chapman <nathan@nchp.mn>
2015-03-12 11:12:03 +1100

    Add styles for blog-article.php

--- site/templates/blog-article.php
+++ site/templates/blog-article.php
@@ -1,17 +1,17 @@
-<?php snippet('header', array('bodyclass' => 'blog-article')) ?>
+<?php snippet('header', array('bodyclass' => 'blog-article blog-single')) ?>
 
 <article>
     <div class="container">
         <div class="row">
-            <div class="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
+            <div class="col-xs-10 col-xs-offset-1 col-md-8 col-md-offset-2 text-center">
                 <h1><?php echo $page->title() ?></h1>
-                <div class="meta col-xs-10 col-xs-offset-1">
-                    <span><?php echo $page->date('j M Y') ?> &mdash; <?php snippet('readingtimeshort', array('text' => $page->text())) ?></span>
+                <div class="meta">
+                    <span><i class="fa fa-pencil"></i> &nbsp;&nbsp;&mdash;&nbsp;&nbsp; <?php echo $page->date('j M Y') ?> &nbsp;&nbsp;&mdash;&nbsp;&nbsp; <?php snippet('readingtimeshort', array('text' => $page->text())) ?></span>
                 </div>
             </div>
         </div>
         <div class="row">
-            <div class="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
+            <div class="col-sm-10 col-sm-offset-1">
                 <div class="leading">
                     <?php echo $page->excerpt()->kirbytext() ?>
                 </div>
