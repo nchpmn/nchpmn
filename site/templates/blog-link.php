9cb86b36c14b7fabb78cb0a15d3a7914b6ee55c3
Nathan Chapman <nathan@nchp.mn>
2015-03-12 11:50:08 +1100

    Changes to width of text columns

--- site/templates/blog-link.php
+++ site/templates/blog-link.php
@@ -11,7 +11,7 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-sm-10 col-sm-offset-1">
+            <div class="col-sm-10 col-sm-offset-1 col-lg-8 col-lg-offset-2">
                 <blockquote>
                     <?php echo $page->quote() ?>
                 </blockquote>
