82a3c5ea4e1070d0862de1dcbe9b9cc8a40dc06f
Nathan Chapman <nathan@nchp.mn>
2015-03-16 18:05:18 +1100

    Very very very basic blog-archive structure

--- site/templates/blog-archive.php
+++ site/templates/blog-archive.php
@@ -1,51 +1,25 @@
 <?php snippet('header', array('bodyclass' => 'blog-archive')) ?>
 
-<header id="cover" class="jumbotron cover">
-        <div class="container text-center">
-            <h1 class="col-xs-6 col-xs-offset-3 col-sm-4 col-sm-offset-4">Blog</h1>
-        </div>        
-</header><!-- End header -->
-
 <div class="container">
-<?php $posts = $page->children()->visible()->flip()->paginate(2) ?>
-<?php $n = 0; foreach($posts as $post): $n++; ?>
-<?php $alignclass = ($n%2) ? 'first-post' : 'second-post' ?>
-<?php if($alignclass == 'first-post'): ?>
-
-    <article class="first-post">
-        <div class="image-container col-xs-3 col-sm-4 col-md-5"
-        <?php $cover = $post->cover(); ?>
-        <?php if($image = $post->image(strval($cover))): ?>
-            style="background-image:url('<?php echo $image->url() ?>');"
-        <?php endif ?>></div>
-        <div class="container">
-            <div class="row">
-                <div class="col-xs-8 col-xs-offset-4 col-sm-7 col-sm-offset-5 col-md-6 col-md-offset-6 col-lg-6 col-lg-offset-6 post-excerpt">
-                    <?php snippet('archive-'.$post->template(), array('post' => $post)) ?>
-                </div>
+    <h1 class="text-center"><span><?php echo $page->title() ?></span></h1>
+    <?php $posts = $page->children()->visible()->flip()->paginate(4) ?>
+    <?php $first = $posts->first() ?>
+    <?php $second = $posts->nth(1) // Second overall item - index starts at 0 ?>
+    <?php foreach($posts as $post): ?>
+        <article class="post first">
+            <h2><?php echo $post->title() ?></h2>
+            <div class="meta">
+                <?php if($post->intendedTemplate() == 'blog-article'): ?>
+                    <i class="fa fa-pencil"></i>&nbsp;&nbsp;&mdash;&nbsp;&nbsp; <?php echo $post->date('j M Y') ?> &nbsp;&nbsp;&mdash;&nbsp;&nbsp; <?php snippet('readingtimeshort', array('text' => $post->text())) ?>
+                <?php elseif($post->intendedTemplate() == 'blog-link'): ?>
+                    <i class="fa fa-link"></i> &nbsp;&nbsp;&mdash;&nbsp;&nbsp; <?php echo $post->date('j M Y') ?>
+                <?php else: ?>
+                    <i class="fa fa-play"></i> &nbsp;&nbsp;&mdash;&nbsp;&nbsp; <?php echo $post->date('j M Y') ?>
+                <?php endif ?>
+                
             </div>
-        </div>
-    </article>
-
-<?php else: ?>
-
-    <article class="second-post">
-        <div class="image-container col-xs-3 col-xs-offset-9 col-sm-4 col-sm-offset-8 col-md-5 col-md-offset-7"
-        <?php $cover = $post->cover(); ?>
-        <?php if($image = $post->image(strval($cover))): ?>
-            style="background-image:url('<?php echo $image->url() ?>');"
-        <?php endif ?>></div>
-        <div class="container">
-            <div class="row">
-                <div class="col-xs-8 col-sm-7 col-md-6 col-lg-6 post-excerpt">
-                    <?php snippet('archive-'.$post->template(), array('post' => $post)) ?>
-                </div>
-            </div>
-        </div>  
-    </article>
-
-<?php endif ?>                    
-<?php endforeach ?>
+        </article>
+    <?php endforeach ?>
 </div>
 
 <?php snippet('pagination-prevnext', array('posts' => $posts)) ?>
