213111691f41b7983e72ec8e39c4325f39df60e9
Nathan Chapman <nathan@nchp.mn>
2015-03-16 19:43:22 +1100

    Fix prevnext.php

--- site/snippets/prevnext.php
+++ site/snippets/prevnext.php
@@ -6,7 +6,7 @@
                 <a class="next col-xs-3 col-xs-offset-3" href="<?php echo $page->prevVisible()->url() ?>"><span class="hidden-xs">PREVIOUS</span><span class="visible-xs">PREV</span></a>
             <?php endif ?>
             <?php if($page->hasNextVisible()): ?>
-                <a class="prev col-xs-3 <?php if($page->hasNextVisible()): ?><?php else: ?>col-xs-offset-6<?php endif; ?>" href="<?php echo $page->nextVisible()->url() ?>">NEXT</a>
+                <a class="prev col-xs-3 <?php if($page->hasPrevVisible()): ?><?php else: ?>col-xs-offset-6<?php endif; ?>" href="<?php echo $page->nextVisible()->url() ?>">NEXT</a>
             <?php endif ?>
             
         </div>
