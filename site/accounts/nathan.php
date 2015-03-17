9b00164dc26d18910ec16f163212c4a5f82f4a91
Nathan Chapman <nathan@nchp.mn>
2015-03-17 19:45:29 +1100

    Add first YouTube video

--- site/accounts/nathan.php
+++ site/accounts/nathan.php
@@ -8,8 +8,8 @@ language: en
 role: admin
 token: 2d6d16e1ce2b3910e3af497c84181d6c96beea31
 history:
+  - extras/youtube/a7s
+  - extras/youtube
   - work
   - extras
   - home/blog
-  - blog/first-blog-link
-  - home/work
