a4c59a60747e2ff5e5b12ccf56a8fa9adbcb49fa
Nathan Chapman <nathan@nchp.mn>
2015-03-17 19:13:37 +1100

    Add redirect template, extras subdirectory

--- site/accounts/nathan.php
+++ site/accounts/nathan.php
@@ -6,10 +6,10 @@ password: >
   $2a$10$YMeRNrw0Z2shQlIph.YrMu.570D.3YjDXhgwdIAkcXs3E1SuPHzXm
 language: en
 role: admin
-token: 6b938e0164938b328229c9b84686c13e40ca6278
+token: 2d6d16e1ce2b3910e3af497c84181d6c96beea31
 history:
+  - extras
   - home/blog
   - blog/first-blog-link
   - home/work
   - blog/video-time
-  - blog/second-blog-article
---
+++ site/blueprints/nope.php
@@ -0,0 +1,12 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+title: Page Redirect
+pages: true
+files: false
+fields:
+  title:
+    label: Title
+    type:  text
+  link:
+    label: Redirect URL
+    type:  url
