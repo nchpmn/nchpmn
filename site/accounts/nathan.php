f9dc83c0e8f773b3cd2108a7f1e167a0f642a8f3
Nathan Chapman <nathan@nchp.mn>
2015-03-18 16:40:27 +1100

    Add more content

--- site/accounts/nathan.php
+++ site/accounts/nathan.php
@@ -6,10 +6,10 @@ password: >
   $2a$10$YMeRNrw0Z2shQlIph.YrMu.570D.3YjDXhgwdIAkcXs3E1SuPHzXm
 language: en
 role: admin
-token: 890aab07c62eecbaf9c352d4f82440a4fc29d766
+token: b57e5bcc4356fa34374b34b2348cf5f9cc43a3d6
 history:
-  - work/illawarra-mercury-your-local-team
-  - home/work
-  - work/morning-run
-  - work
-  - work/the-fighter
+  - work/wollongong-adventure-weekend
+  - blog/this-is-our-year-behind-the-scenes
+  - work/this-is-our-year
+  - work/uow-open-day
+  - work/gpt-wollongong-central
