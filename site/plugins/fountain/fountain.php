fe9b13ab0bebc6fe2b4e5fe2cb14230104754d5e
Nathan Chapman <nathan@nchp.mn>
2015-03-26 11:37:19 +1100

    Plugin - this is beyond my skill

---
+++ site/plugins/fountain/fountain.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Fountain
+ *
+ * Sample Usage:
+ *
+ * <?php echo $page->text()->fountain() ?>
+ *
+ * @author Nathan Chapman
+ * @version 0.0.0 Dev
+ */
+ 
+function fountain($script) {
+    $test = 'hello';
+    return $test;
+}
+?>
--- site/tags/fountain.php
+++ site/tags/fountain.php
@@ -1,10 +1,11 @@
 <?php
 
 kirbytext::$tags['fountain'] = array(
-  'html' => function($tag) {
-    $script = $tag->file($tag->attr('fountain'))->read();
-    return js('assets/js/fountain/fountain.js');
-      }
+    'html' => function($tag) {
+        echo "<!-- FOUNTAIN -->";
+        return $tag->file($tag->attr('fountain'))->fountain();
+        echo "<!-- FOUNTAIN END -->";
+    }
 );
 
 ?>
