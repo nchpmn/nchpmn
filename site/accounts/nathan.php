1e4b5607c3dbb8d804f7bd5067af774843d945c8
Nathan Chapman <nathan@nchp.mn>
2015-03-26 15:30:34 +1100

    Fix stupid mistake - MAKE CHANGES LOCALLY, NOT ON SERVER
    You idiot, Nathan

--- site/accounts/nathan.php
+++ site/accounts/nathan.php
@@ -6,10 +6,10 @@ password: >
   $2a$10$YMeRNrw0Z2shQlIph.YrMu.570D.3YjDXhgwdIAkcXs3E1SuPHzXm
 language: en
 role: admin
-token: 554bc52644c63780c4120a50f36ba0b06fa5c562
+token: e952641f4242b2a3ab0ae84a6204467cf1086c06
 history:
+  - work/family-building
   - work/uow-open-day
   - work/on-crown
   - work/morning-run
-  - work/family-building
   - work/the-fighter
