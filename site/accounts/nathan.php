1784f394f5933bbf848f09646dd5857f82215bfe
Nathan Chapman <nathan@nchp.mn>
2015-03-26 11:12:26 +1100

    Create default structure

--- site/accounts/nathan.php
+++ site/accounts/nathan.php
@@ -6,10 +6,10 @@ password: >
   $2a$10$YMeRNrw0Z2shQlIph.YrMu.570D.3YjDXhgwdIAkcXs3E1SuPHzXm
 language: en
 role: admin
-token: 554bc52644c63780c4120a50f36ba0b06fa5c562
+token: 3798258d3df74d1c7280caccc63fd4781d0ff59e
 history:
+  - blog/fountain
   - work/uow-open-day
   - work/on-crown
   - work/morning-run
   - work/family-building
-  - work/the-fighter
