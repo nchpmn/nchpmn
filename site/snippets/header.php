188891e204d88bbeea843880258075d4bf3ba204
Nathan Chapman <nathan@nchp.mn>
2015-03-31 10:28:53 +1100

    Add Pingdom RUM user monitoring - TEST ONLY

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -29,6 +29,17 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+    <script>
+var _prum = [['id', '5519da9eabe53dc12b31abbc'],
+             ['mark', 'firstbyte', (new Date()).getTime()]];
+(function() {
+    var s = document.getElementsByTagName('script')[0]
+      , p = document.createElement('script');
+    p.async = 'async';
+    p.src = '//rum-static.pingdom.net/prum.min.js';
+    s.parentNode.insertBefore(p, s);
+})();
+</script>
 </head>
 <body class="<?php echo $bodyclass ?>">
 <?php snippet('navbar') ?>
