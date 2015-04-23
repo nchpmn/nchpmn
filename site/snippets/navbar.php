495497e8ea1f8b535c864d9246584cd794aa0b45
Nathan Chapman <nathan@nchp.mn>
2015-04-23 11:25:20 +1000

    Add home link to logo

--- site/snippets/navbar.php
+++ site/snippets/navbar.php
@@ -6,7 +6,7 @@
                 <span class="sr-only">Toggle Navigation</span>
                 <i class="fa fa-asterisk"></i>
             </button>
-            <a class="navbar-brand" href="#">NCHP<span class="orange"><strong>.</strong></span>MN</a>
+            <a class="navbar-brand" href="<?php echo $site->url() ?>">NCHP<span class="orange"><strong>.</strong></span>MN</a>
         </div>
         
         <div class="collapse navbar-collapse clearfix" id="collapse">
