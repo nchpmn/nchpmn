d48588a9bbeb088d533734576584cb37a1f5b01d
Nathan Chapman <nathan@nchp.mn>
2015-05-06 12:53:12 +1000

    Change Home / Quals - Wrong column size on col-xs

--- site/snippets/home-quals.php
+++ site/snippets/home-quals.php
@@ -3,7 +3,7 @@
         <div class="row">
             <h3 class="text-center hidden-xs"><span><?php echo $data->title() ?></span></h3>
             <h3 class="text-center visible-xs"><span><?php echo $data->shorttitle() ?></span></h3>
-            <div class="col-xs-8 col-xs-offset-2 col-sm-6 col-sm-offset-0">
+            <div class="col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-0">
                 <dl class="quals-major">
                     <?php foreach($data->mainquals()->yaml() as $item): ?>
                         <dt><?php echo $item['award'] ?></dt>
@@ -11,7 +11,7 @@
                     <?php endforeach ?>
                 </dl>
             </div>
-            <div class="col-xs-8 col-xs-offset-2 col-sm-6 col-sm-offset-0">
+            <div class="col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-0">
                 <dl class="quals-minor">                
                     <?php foreach($data->minorquals()->yaml() as $item): ?>
                         <dt><?php echo $item['award'] ?></dt>
