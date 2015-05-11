5767918b823915d16064673e4d0ea6a00e89c80f
Nathan Chapman <nathan@nchp.mn>
2015-05-11 15:32:17 +1000

    Fix responsive Embeds - Use Default classes

--- site/snippets/project-single-ajax.php
+++ site/snippets/project-single-ajax.php
@@ -2,15 +2,15 @@
     <div class="col-sm-6 col-md-7 media" style="display:block">
     <?php $cover = $page->cover(); ?>
     <?php if($page->videoSource() == "youtube"): ?>
-        <!-- YOUTUBE -->
-        <div class="video-responsive item">
-            <iframe width="1280" height="720" src="https://www.youtube.com/embed/<?php echo $page->videoID(); ?>?showinfo=0&autohide=1&autoplay=0&controls=2&modestbranding=1" frameborder="0" allowfullscreen></iframe>
+        <!-- YOUTUBE -->        
+        <div class="embed-responsive embed-responsive-16by9">
+            <iframe class="embed-responsive-item" width="1280" height="720" src="https://www.youtube.com/embed/<?php echo $page->videoID(); ?>?showinfo=0&autohide=1&autoplay=0&controls=2&modestbranding=1" frameborder="0" allowfullscreen></iframe>
         </div>
     <?php elseif($page->videoSource() == "vimeo"): ?>
         <!-- VIMEO -->
-        <div class="video-responsive item">
-            <iframe src="http://player.vimeo.com/video/<?php echo $page->videoID(); ?>?portrait=0&title=0&badge=0&byline=0&color=ed8046" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
-        </div>    
+        <div class="embed-responsive embed-responsive-16by9">
+            <iframe class="embed-responsive-item" src="http://player.vimeo.com/video/<?php echo $page->videoID(); ?>?portrait=0&title=0&badge=0&byline=0&color=ed8046" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+        </div>
     <?php elseif($image = $page->image(strval($cover))): ?>
         <a href="<?php echo $page->url() ?>"><img class="img-responsive" src="<?php echo $image->url() ?>"></a>
     <?php endif ?>
